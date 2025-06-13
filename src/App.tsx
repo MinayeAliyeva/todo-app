abbaa0c8577e1e5dccf4d8fc9bf1bf3f834bf59e
MinayeAliyeva <7s346pu@code.edu.az>
2025-06-13 14:36:16 +0400

    fix:config resolve

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,101 @@
 import FormComp from "./components/FormComp";
 import "./App.css";
 import TableComp from "./components/TableComp";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import type { IUser } from "./type";
 import { Form } from "react-bootstrap";
+
 export interface IState {
-  user: IUser;
+  users: IUser[];
   searchText: string;
+  copyUsers: IUser[];
 }
+
 const App = () => {
   const [state, setState] = useState<IState>({
-    user: {
-      name: "",
-      sname: "",
-      gender: "",
-      check: false,
-      city: "",
-      id:""
-    },
+    users: [
+      {
+        name: "Minaya",
+        sname: "Aliyeva",
+        gender: "female",
+        check: true,
+        city: "Baku",
+        id: "1",
+      },
+    ],
+    copyUsers: [],
     searchText: "",
   });
 
+  const [editedUser, setEditedUser] = useState<IUser | null>(null);
   const onAddUser = (user: IUser) => {
+    const exsistedUser = state.users.find((item) => item.id === user.id);
     setState((prevState) => ({
       ...prevState,
-      user,
+
+      users: exsistedUser
+        ? prevState.users.map((item) => {
+            if (item.id === exsistedUser.id) {
+              return user;
+            }
+            return item;
+          })
+        : [...prevState.users, user],
+      copyUsers: exsistedUser
+        ? prevState.users.map((item) => {
+            if (item.id === exsistedUser.id) {
+              return user;
+            }
+            return item;
+          })
+        : [...prevState.users, user],
     }));
   };
-  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    setState((prevState) => ({ ...prevState, searchText: event.target.value }));
+
+  const onEditUser = (user: IUser) => {
+    setEditedUser(user);
+  };
+  const handleSearchChange: React.ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    const searchedUsers = state.users.filter(
+      (user) =>
+        user.name.toLowerCase().includes(event.target.value.toLowerCase()) ||
+        user.sname.toLowerCase().includes(event.target.value.toLowerCase()) ||
+        user.city.toLowerCase().includes(event.target.value.toLowerCase()) ||
+        user.gender.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    console.log({ searchedUsers, value: event.target.value });
+
+    setState((prevState) => ({
+      ...prevState,
+      users: event.target.value ? searchedUsers : state.copyUsers,
+    }));
+  };
+  const onDeleteUser = (id: string) => {
+    setState((prevState) => ({
+      ...prevState,
+      users: prevState.users.filter((user) => user.id !== id),
+    }));
   };
 
-  
   return (
     <>
-      <FormComp onAddUser={onAddUser} />
+      <FormComp onAddUser={onAddUser} userToEdit={editedUser} />
       <Form.Control
         type="text"
         id="search"
         style={{ margin: "20px", width: "250px" }}
-        placeholder="Search By Name"
-        onChange={handleChange}
+        placeholder="Arama Yap"
+        onChange={handleSearchChange}
         name="search"
       />
-      <TableComp  user={state.user} searchText={state.searchText}  />
+      <TableComp
+        users={state.users}
+        searchText={state.searchText}
+        onEditUser={onEditUser}
+        onDeleteUser={onDeleteUser}
+      />
     </>
   );
 };
--- src/components/TableComp.tsx
+++ src/components/TableComp.tsx
@@ -1,56 +1,27 @@
 import Table from "react-bootstrap/Table";
-import { memo, useEffect, useState } from "react";
+import { memo } from "react";
 import type { IUser } from "../type";
-import { v4 as uuid } from "uuid";
 import { Button } from "react-bootstrap";
 interface ITableProps {
-  user: IUser;
-  searchText: string;
-}
-interface ITableState {
   users: IUser[];
-  editedUser?: IUser | null;
+  searchText: string;
+  onEditUser: (user: IUser) => void;
+  onDeleteUser: (id: string) => void;
 }
-const TableComp = ({ user, searchText }: ITableProps) => {
-  const [state, setState] = useState<ITableState>({
-    users: [
-      {
-        name: "Minaya",
-        sname: "Aliyeva",
-        city: "Baku",
-        gender: "female",
-        check: true,
-        id: "1",
-      },
-    ],
-    editedUser: null,
-  });
-  useEffect(() => {
-    if (user.name && user.sname) {
-      setState((prevState) => ({
-        ...prevState,
-        users: [...prevState.users, { ...user, id: uuid() }],
-      }));
-    }
-  }, [user]);
-  const filteredUsers = state.users.filter(
+
+const TableComp = ({
+  users,
+  searchText,
+  onEditUser,
+  onDeleteUser,
+}: ITableProps) => {
+  const filteredUsers = users.filter(
     (user) =>
       user.name.toLowerCase().includes(searchText.toLowerCase()) ||
       user.sname.toLowerCase().includes(searchText.toLowerCase()) ||
       user.city.toLowerCase().includes(searchText.toLowerCase()) ||
       user.gender.toLowerCase().includes(searchText.toLowerCase())
   );
-
-  const onDeleteUser = (id: string) => {
-    setState((prevState) => ({
-      ...prevState,
-      users: prevState.users.filter((user) => user.id !== id),
-    }));
-  };
-  const onEditUser = (id: string) => {
-    const userToEdit = state.users.find((user) => user.id === id);
-    setState((prevState) => ({ ...prevState, editedUser: userToEdit }));
-  };
 
   return (
     <Table striped bordered hover style={{ marginTop: "50px" }}>
@@ -66,7 +37,7 @@ const TableComp = ({ user, searchText }: ITableProps) => {
         </tr>
       </thead>
       <tbody>
-        {filteredUsers?.map((user, index) => {
+        {users?.map((user, index) => {
           return (
             <tr key={index}>
               <td>{index}</td>
@@ -87,7 +58,7 @@ const TableComp = ({ user, searchText }: ITableProps) => {
                   children="Edit "
                   size="lg"
                   variant="primary"
-                  onClick={() => onEditUser(user.id)}
+                  onClick={() => onEditUser(user)}
                 />
               </td>
             </tr>
